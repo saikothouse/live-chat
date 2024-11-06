bd942d2cf323a8ce4733bc9fd501f5fa71eb9082
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-06 21:35:09 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDXkIMxlRbEU2DyNYrgVrwNywPSxKgmyH4",
+  authDomain: "binlistio.firebaseapp.com",
+  databaseURL: "https://binlistio-default-rtdb.firebaseio.com",
+  projectId: "binlistio",
+  storageBucket: "binlistio.appspot.com",
+  messagingSenderId: "10459136687",
+  appId: "1:10459136687:web:0b614249a85b0297af0a5a",
+  measurementId: "G-HMH7WT44TT"
 };
 
 const app = initializeApp(firebaseConfig);
