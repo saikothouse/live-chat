c827091a11b959f3d3850f497e34c80cbb13c77f
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-06 21:37:21 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,14 @@
 import "./globals.css";
 
-export default function Layout({ children }) {
-    return (
-        <div className="min-h-screen bg-gray-100">
-            {children}
-        </div>
-    );
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="bg-gray-100 font-roboto">
+        {children}
+      </body>
+    </html>
+  );
 }
